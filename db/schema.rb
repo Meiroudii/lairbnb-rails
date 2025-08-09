0d8a0c593e5e1ff848632a47011ecd8997fd1fdc
Meiroudii <119019700+Meiroudii@users.noreply.github.com>
2025-08-09 22:40:15 +0900

    feat: add house info fields to listings

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_08_09_104615) do
+ActiveRecord::Schema[8.0].define(version: 2025_08_09_133752) do
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
     t.text "body"
@@ -54,6 +54,9 @@ ActiveRecord::Schema[8.0].define(version: 2025_08_09_104615) do
     t.string "address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "bedrooms"
+    t.integer "bathrooms"
+    t.integer "people_limit"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
